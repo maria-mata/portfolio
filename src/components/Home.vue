0f08ef130056a6434c385967907c97bfe65cfa9a
Maria Mata <mariajo.mata@gmail.com>
2017-09-30 14:11:12 -0600

    project setup 📂

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div>
+    <!-- <img src="../assets/maria-logo.png" width="200"> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="css">
+</style>
