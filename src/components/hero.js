88a7b2b89fd24a5de0733037489a7d5dd6ffc989
Maria Mata <25022665+maria-mata@users.noreply.github.com>
2023-08-13 16:58:05 -0600

    Add headshot

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import './hero.scss';
 
 export default function Hero() {
   return (
     <section className="hero">
-      <h1><span aria-label="hello">👋</span> I'm Maria,</h1>
-      <h2>I build beautiful and accessible products</h2>
-      <p>Welcome to my website, where I share some of my work focused on helping people do their best work. I'm currently searching for my next remote role and have outlined <Link>a few reasons</Link> why you may want me on your team. Let's connect!</p>
+      <div className="hero__image">
+        <StaticImage 
+          src="../images/mata-mariajose-headshot.jpeg"
+          alt="Maria Jose Mata, Software Engineer"
+          layout="fixed"
+          width={250}
+          height={250}
+        />
+      </div>
+      <div className="hero__content">
+        <h1>I'm Maria <span aria-label="Hello">👋</span></h1>
+        <h2>I build beautiful, responsive and accessible web products</h2>
+        <p>Welcome to my website, where I share examples of my work. I'm currently searching for my next remote role and have outlined <Link>a few reasons</Link> why you may want me on your team. Let's connect!</p>
+      </div>
     </section>
   );
 }
