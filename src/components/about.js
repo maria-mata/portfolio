7b05cd53897089c90fdb543acd93bbc4ffccb27b
Maria Mata <25022665+maria-mata@users.noreply.github.com>
2023-08-19 11:20:27 -0600

    Remove about image

--- src/components/about.js
+++ src/components/about.js
@@ -1,8 +1,6 @@
 import React, { } from 'react';
 import Section from './section';
 import { graphql, useStaticQuery } from 'gatsby';
-import { StaticImage } from 'gatsby-plugin-image';
-import ImageWrapper from './imageWrapper';
 import './about.scss';
 
 // TODO: figure out mobile image resizing with JS
@@ -20,17 +18,6 @@ export default function About() {
     <Section title="About" sectiondId="about">
       <div className="about">
         <div dangerouslySetInnerHTML={{ __html: html }} />
-        <div>
-          <ImageWrapper>
-            <StaticImage 
-              src="../images/sunrise-longs-peak.jpg"
-              alt="Sunrise at Longs Peak, Colorado."
-              width={300}
-              height={400}
-            />
-          </ImageWrapper>
-          <small>Photo by Greg.</small>
-        </div>
       </div>
     </Section>
   );
