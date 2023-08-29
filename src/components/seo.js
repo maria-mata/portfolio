d1ba9a0599b3a110e16315ed7a477f5d2ef7edb6
Maria Mata <25022665+maria-mata@users.noreply.github.com>
2023-08-29 15:51:00 -0600

    Fix console errors, add html lang attribute

--- src/components/seo.js
+++ src/components/seo.js
@@ -13,6 +13,8 @@ const SEO = ({ title, description, pathname, children }) => {
 
   return (
     <>
+      <html lang="en" />
+      
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
@@ -20,7 +22,7 @@ const SEO = ({ title, description, pathname, children }) => {
       {children}
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&family=Ubuntu:wght@700&display=swap" rel="stylesheet" />
     </>
   )
